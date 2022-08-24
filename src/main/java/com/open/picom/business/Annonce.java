013982e9c1a3e103dc922afc07b345bdf5c99378
Mad <madiane.lenigen@protonmail.com>
2022-08-24 17:20:25 +0200

    I forgot to set up a dev branch, just fixed. Also commiting annonce class

--- src/main/java/com/open/picom/business/Annonce.java
+++ src/main/java/com/open/picom/business/Annonce.java
@@ -1,5 +1,37 @@
 package com.open.picom.business;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+
 public class Annonce {
-    
+    private Long id; 
+    private LocalDateTime dateHeureCreation; 
+    private LocalDateTime dateHeureDebut;
+    private LocalDateTime dateHeureFin;
+    private String contenu; 
+    private String titre;
+    private String numeroCarte; 
+    private int anneExpiration; 
+    private byte moisExpiration;
+    private String cryptogramme; 
+    private double montantRegleEnEuros; 
+    private List<Zone> zones = new ArrayList<Zone>();
+    private List<TrancheHorraire> trancheHorraires = new ArrayList<TrancheHorraire>();
+    private Client client; 
+
 }
